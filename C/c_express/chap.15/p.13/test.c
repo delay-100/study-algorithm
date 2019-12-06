af1b77f33f1ef97f642c20a12fff420d66901ebc
paekjiyeon <qor3127@naver.com>
2019-12-07 02:27:57 +0900

    fin c_express chap 15
    p.13

--- C/c_express/chap.15/p.13/test.c
+++ C/c_express/chap.15/p.13/test.c
@@ -6,6 +6,10 @@
 // 파일이 여러 줄일 경우 줄의 앞에 띄어쓰기가 됨
 //                     마지막 단어만 바뀜
 
+// 수정 완료
+// fgets에서 자동으로 입력되는 버퍼를 삭제해줌
+// 그 후 fputs로 다시 개행문자를 추가해줌
+
 #include <stdio.h>
 #include <string.h>
 
@@ -24,26 +28,31 @@ int main(void){
     scanf("%s", rw);
     printf("\nwant word: ");
     scanf("%s", ww);
-
+    
     while(!feof(f)){
 
         fgets(buffer, SIZE, f);
+        buffer[strlen(buffer)-1] = '\0';
 
-        token = strtok(buffer, " ");
+        if(strstr(buffer, rw)!=NULL){
+            token = strtok(buffer, " ");
 
-        while(token != NULL){
-            if(strcmp(token, rw) ==0){
-                fputs(ww, r);
+            while(token != NULL){
+                if(strcmp(token, rw) ==0){
+                    fputs(ww, r);
+                }
+                else
+                    fputs(token, r);
+                fputs(" ", r);
 
+                token = strtok(NULL, " ");
             }
-            else{
-                fputs(token, r);
-
-            }
-            fputs(" ", r);
-            token = strtok(NULL, " ");
-
         }
+        else
+        {
+            fputs(buffer, r);
+        }
+        fputs("\n", r);
     }
 
     fclose(f);

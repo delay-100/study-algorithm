95e3d87ba12af835a7bd27715aadc136ea52a034
paekjiyeon <qor3127@naver.com>
2020-01-11 01:21:20 +0900

    codeforces Round613_div2
    A.JustEatIt

---
+++ Java/codeforces/src/Round613_div2/MezoPlayingZoma.java
@@ -0,0 +1,16 @@
+package Round613_div2;
+
+import java.util.Scanner;
+
+public class MezoPlayingZoma {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		String s = input.next();
+		
+		System.out.println(s.length()+1);
+		input.close();
+	}
+}

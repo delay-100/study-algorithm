1075982b364513181393c12e89efaed344318e52
delay100 <qor3127@naver.com>
2022-06-12 13:57:32 +0900

    add frame - bufferedreader

---
+++ Java/frame/bufferedreader.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(bf.readLine());
+		
+		while (true) {
+			String i = bf.readLine();
+
+		}
+			
+		
+
+	}
+}

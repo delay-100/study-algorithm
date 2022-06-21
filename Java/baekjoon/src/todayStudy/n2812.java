551c25c452facc32a911acdb82564d2bb5e28d12
delay100 <qor3127@naver.com>
2022-06-21 23:07:23 +0900

    amm..

---
+++ Java/baekjoon/src/todayStudy/n2812.java
@@ -0,0 +1,29 @@
+package todayStudy;
+
+import java.util.Scanner;
+
+public class n2812 {
+	public static void main(String[] args)  {
+		Scanner input = new Scanner(System.in);
+		
+		int N = input.nextInt();
+		int K = input.nextInt();
+		
+		String num = input.next();
+		
+		String[] templist = num.split("");
+		int[] list = new int[N];
+		int temp = list[0];
+		
+		for(int i=0; i<N; i++) {
+			list[i] = Integer.parseInt(templist[i]);
+		}
+		
+		for(int i=0; i<N; i++) {
+			
+		}
+		
+		
+		input.close();
+	}
+}
